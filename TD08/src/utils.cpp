dc9044be574ca523f0309b93684f6b0f3158e378
LisaMrc <lisa.muracciole77@gmail.com>
2023-12-19 11:50:29 +0100

    ⚡ TD08

--- TD08/src/utils.cpp
+++ TD08/src/utils.cpp
@@ -1,45 +1,14 @@
 #include "fraction.hpp"
 #include <numeric>
 
-int gcd(int pos_int1, int pos_int2)
+unsigned int gcd(unsigned int a, unsigned int b) 
 {
-    int pos_int1_is_greater {(pos_int1 > pos_int2) ? 1 : 0 };
-
-    int div_result{};
-
-    switch (pos_int1_is_greater)
-    {
-    case 1:
-        div_result = pos_int1 % pos_int2;
-        
-        while (div_result != 0);
-        {
-            int div_result{pos_int1 % pos_int2};
-            pos_int1 = pos_int2;
-            pos_int2 = div_result;
-        }
-        
-        break;
-    
-    default:
-        div_result = pos_int2 % pos_int1;
-        
-        while (div_result != 0);
-        {
-            int div_result{pos_int2 % pos_int1};
-            pos_int2 = pos_int1;
-            pos_int1 = div_result;
-        }
-        
-        break;
-    }
-
-    return div_result;
+    return b == 0 ? a : gcd(b, a % b);
 }
 
 Fraction simplify(Fraction const& f)
 {
-    unsigned divisor{std::gcd(f.numerator, f.denominator)};
+    unsigned divisor{gcd(f.numerator, f.denominator)};
 
     return {
         f.denominator/divisor,
