77b4ac6c6d487531f26da888fc19c3c9672fcc84
LisaMrc <lisa.muracciole77@gmail.com>
2024-02-27 22:22:25 +0100

    🧹 Cleanup !

--- TD08 Correction/build/CMakeFiles/3.27.8/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "C:/winlibs-x86_64-posix-seh-gcc-13.2.0-llvm-17.0.5-mingw-w64ucrt-11.0.1-r3/mingw64/bin/windres.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .obj)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
--- TD08 Correction/build/CMakeFiles/main.dir/src/utils.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/src/utils.cpp.obj: \
- C:\IMAC\ 1\Programmation\ et\ Algorithmique\IMAC_1_TDs_CPP\TD08\ Correction\src\utils.cpp \
- C:\IMAC\ 1\Programmation\ et\ Algorithmique\IMAC_1_TDs_CPP\TD08\ Correction\src\utils.hpp \
- C:\IMAC\ 1\Programmation\ et\ Algorithmique\IMAC_1_TDs_CPP\TD08\ Correction\src\fraction.hpp
